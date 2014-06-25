827e448a41a059d15873546eb6a916f950eb5bd4
Matt Harrison <mt.harrison86@gmail.com>
2014-06-25 20:12:08 +0100

    Added new events

--- event-specs.js
+++ event-specs.js
@@ -19,6 +19,46 @@ var eventSpecs = {
 		process_name: "Vpapi initting game",
 		process_bound: "end"
 	},
+	"vpapiDealRequested": {
+		domain: "network/api",
+		process_name: "Vpapi dealing",
+		process_bound: "start"
+	},
+	"vpapiDealResponseReceived": {
+		domain: "network/api",
+		process_name: "Vpapi dealing",
+		process_bound: "end"
+	},
+	"vpapiDrawRequested": {
+		domain: "network/api",
+		process_name: "Vpapi drawing",
+		process_bound: "start"
+	},
+	"vpapiDrawResponseReceived": {
+		domain: "network/api",
+		process_name: "Vpapi drawing",
+		process_bound: "end"
+	},
+	"vpapiPlayerinfoRequested": {
+		domain: "network/api",
+		process_name: "Vpapi getting player info",
+		process_bound: "start"
+	},
+	"vpapiPlayerinfoResponseReceived": {
+		domain: "network/api",
+		process_name: "Vpapi getting player info",
+		process_bound: "end"
+	},
+	"vpapiEndgameRequested": {
+		domain: "network/api",
+		process_name: "Vpapi ending game",
+		process_bound: "start"
+	},
+	"vpapiEndgameResponseReceived": {
+		domain: "network/api",
+		process_name: "Vpapi ending game",
+		process_bound: "end"
+	},
 };
 
 module.exports = eventSpecs;
