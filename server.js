3321b9ec58589180b8c919959a33cf764eb78da6
Matt Harrison <hi@matt-harrison.com>
2015-01-19 10:29:25 +0000

    Allow X-domain

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ function rawBody(req, res, next) {
 }
 
 var allowCrossDomain = function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type');
 
