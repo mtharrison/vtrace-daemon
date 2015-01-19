c2f9434c4d2ffe091d58a5b226ff34e73be68afa
Matt Harrison <hi@matt-harrison.com>
2015-01-19 10:27:45 +0000

    Allow X-domain

--- server.js
+++ server.js
@@ -19,6 +19,16 @@ function rawBody(req, res, next) {
   });
 }
 
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', 'example.com');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+}
+
+app.use(allowCrossDomain);
+
 function getUsername(sessionid, cb) {
 	var baseURL = 'http://www.videopoker.com/api/session_info?sessionid=';
 	request(baseURL + sessionid, function(error, response, body){
